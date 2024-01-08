16b6f09e370ec4d65889032f6e2b18e8344ac2ed
ariefff666 <mariefpratama90@gmail.com>
2024-01-08 07:00:48 +0700

    homepage + search

--- lib/main.dart
+++ lib/main.dart
@@ -29,6 +29,12 @@ class MyHomePage extends StatefulWidget {
 class _MyHomePageState extends State<MyHomePage> {
   int _currentIndex = 0;
   bool _isSearchActive = false;
+  double _quickBallPositionY = 30; // Y position of Quick Ball
+
+  double _iconStartPositionX = 0;
+  double _lastDragPositionX = 0;
+  double _iconStartPositionY = 0;
+  double _lastDragPositionY = 0;
 
   @override
   Widget build(BuildContext context) {
@@ -47,8 +53,155 @@ class _MyHomePageState extends State<MyHomePage> {
       ),
       floatingActionButton: Stack(
         children: [
-          if (_isSearchActive) _buildSearchIcons(),
-          _buildSearchButton(),
+          Positioned(
+            bottom: _quickBallPositionY,
+            left: 0,
+            right: 0,
+            child: GestureDetector(
+              onTap: () {
+                setState(() {
+                  _isSearchActive = !_isSearchActive;
+                });
+              },
+              onPanStart: (details) {
+                setState(() {
+                  _isSearchActive = true;
+                  _iconStartPositionX = details.localPosition.dx;
+                  _iconStartPositionY = details.localPosition.dy;
+                });
+              },
+              onPanUpdate: (details) {
+                _lastDragPositionX = details.localPosition.dx;
+                _lastDragPositionY = details.localPosition.dy;
+                setState(() {});
+              },
+              onPanEnd: (details) {
+                setState(() {
+                  _isSearchActive = false;
+                  _quickBallPositionY = 30;
+
+                  double distanceX = _lastDragPositionX - _iconStartPositionX;
+                  double distanceY = _lastDragPositionY - _iconStartPositionY;
+
+                  if (distanceX.abs() > 30 || distanceY.abs() > 20) {
+                    if (_lastDragPositionX < _iconStartPositionX - 35 &&
+                        _lastDragPositionY < _iconStartPositionY &&
+                        _lastDragPositionY < _iconStartPositionY) {
+                      Navigator.push(
+                        context,
+                        MaterialPageRoute(builder: (context) => SearchPage()),
+                      );
+                    } else if (_lastDragPositionY < _iconStartPositionY - 50 &&
+                        _lastDragPositionX - 50 < _iconStartPositionX &&
+                        _lastDragPositionX + 50 > _iconStartPositionX) {
+                      Navigator.push(
+                        context,
+                        MaterialPageRoute(builder: (context) => EmojiPage()),
+                      );
+                    } else if (_lastDragPositionX > _iconStartPositionX + 35 &&
+                        _lastDragPositionY < _iconStartPositionY &&
+                        _lastDragPositionY < _iconStartPositionY - 10) {
+                      Navigator.push(
+                        context,
+                        MaterialPageRoute(builder: (context) => MusicPage()),
+                      );
+                    }
+                  }
+                });
+              },
+              child: Container(
+                height: 60,
+                width: 60,
+                decoration: const BoxDecoration(
+                  shape: BoxShape.circle,
+                  color: Colors.blue,
+                ),
+                child: const Center(
+                  child: Icon(
+                    Icons.search,
+                    color: Colors.white,
+                    size: 35,
+                  ),
+                ),
+              ),
+            ),
+          ),
+          AnimatedPositioned(
+            duration: const Duration(milliseconds: 200),
+            curve: Curves.easeInOut,
+            bottom: _isSearchActive ? 60 : -100,
+            left: 0,
+            right: 0,
+            child: Container(
+              height: 100,
+              width: MediaQuery.of(context).size.width, // Full width container
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                children: [
+                  Container(
+                    height: 50,
+                    width: 50,
+                    decoration: BoxDecoration(
+                      shape: BoxShape.circle,
+                      color: Colors.cyan,
+                    ),
+                    child: IconButton(
+                      icon: Icon(Icons.emoji_emotions, color: Colors.white),
+                      onPressed: () {
+                        // Handle action for search icon
+                        Navigator.push(
+                          context,
+                          MaterialPageRoute(builder: (context) => EmojiPage()),
+                        );
+                      },
+                    ),
+                  ),
+                  Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                      children: [
+                        Container(
+                          height: 50,
+                          width: 50,
+                          decoration: BoxDecoration(
+                            shape: BoxShape.circle,
+                            color: Colors.cyan,
+                          ),
+                          child: IconButton(
+                            icon: Icon(Icons.search, color: Colors.white),
+                            onPressed: () {
+                              // Handle action for search icon
+                              Navigator.push(
+                                context,
+                                MaterialPageRoute(
+                                    builder: (context) => SearchPage()),
+                              );
+                            },
+                          ),
+                        ),
+                        Container(
+                          height: 50,
+                          width: 50,
+                          decoration: BoxDecoration(
+                            shape: BoxShape.circle,
+                            color: Colors.cyan,
+                          ),
+                          child: IconButton(
+                            icon: Icon(Icons.music_note, color: Colors.white),
+                            onPressed: () {
+                              // Handle action for search icon
+                              Navigator.push(
+                                context,
+                                MaterialPageRoute(
+                                    builder: (context) => MusicPage()),
+                              );
+                            },
+                          ),
+                        ),
+                      ]),
+                ],
+              ),
+            ),
+          ),
         ],
       ),
       floatingActionButtonLocation: FloatingActionButtonLocation.centerDocked,
@@ -66,109 +219,6 @@ class _MyHomePageState extends State<MyHomePage> {
     }
   }
 
-  Widget _buildSearchButton() {
-    return Positioned(
-      bottom: 30,
-      left: 0,
-      right: 0,
-      child: GestureDetector(
-        onTap: () {
-          setState(() {
-            _isSearchActive = !_isSearchActive;
-          });
-        },
-        child: Container(
-          height: 60,
-          width: 60,
-          decoration: const BoxDecoration(
-            shape: BoxShape.circle,
-            color: Colors.blue,
-          ),
-          child: const Center(
-            child: Icon(
-              Icons.search,
-              color: Colors.white,
-              size: 35,
-            ),
-          ),
-        ),
-      ),
-    );
-  }
-
-  Widget _buildSearchIcons() {
-    return AnimatedPositioned(
-      duration: const Duration(milliseconds: 6000),
-      curve: Curves.easeInOut,
-      bottom: _isSearchActive ? 80 : 0,
-      left: 0,
-      right: 0,
-      child: Container(
-        height: 100,
-        width: 100,
-        child: Row(
-          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-          children: [
-            Container(
-              height: 50,
-              width: 50,
-              decoration: BoxDecoration(
-                shape: BoxShape.circle,
-                color: Colors.cyan,
-              ),
-              child: IconButton(
-                icon: Icon(Icons.search, color: Colors.white),
-                onPressed: () {
-                  // Handle action for search icon
-                  Navigator.push(
-                    context,
-                    MaterialPageRoute(builder: (context) => SearchPage()),
-                  );
-                },
-              ),
-            ),
-            Container(
-              height: 50,
-              width: 50,
-              decoration: BoxDecoration(
-                shape: BoxShape.circle,
-                color: Colors.cyan,
-              ),
-              child: IconButton(
-                icon: Icon(Icons.emoji_emotions, color: Colors.white),
-                onPressed: () {
-                  // Handle action for search icon
-                  Navigator.push(
-                    context,
-                    MaterialPageRoute(builder: (context) => EmojiPage()),
-                  );
-                },
-              ),
-            ),
-            Container(
-              height: 50,
-              width: 50,
-              decoration: BoxDecoration(
-                shape: BoxShape.circle,
-                color: Colors.cyan,
-              ),
-              child: IconButton(
-                icon: Icon(Icons.music_note, color: Colors.white),
-                onPressed: () {
-                  // Handle action for search icon
-                  Navigator.push(
-                    context,
-                    MaterialPageRoute(builder: (context) => MusicPage()),
-                  );
-                },
-              ),
-            ),
-          ],
-        ),
-      ),
-    );
-  }
-
   void _onTabTapped(int index) {
     setState(() {
       _currentIndex = index; // Update indeks saat tab lainnya dipilih
@@ -181,8 +231,10 @@ class _MyHomePageState extends State<MyHomePage> {
 class HomeScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Center(
-      child: Text('Home Screen'),
+    return Scaffold(
+      body: Center(
+        child: Text('Home Page'),
+      ),
     );
   }
 }
@@ -190,8 +242,10 @@ class HomeScreen extends StatelessWidget {
 class ProfileScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Center(
-      child: Text('Profile Screen'),
+    return Scaffold(
+      body: Center(
+        child: Text('Profile Page'),
+      ),
     );
   }
 }
