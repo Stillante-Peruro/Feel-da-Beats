3394441fda27a17d5924b8d056a8bf3de3451ae8
widya <widyadhana92@gmail.com>
2024-01-16 16:31:30 +0700

    upgrade design login&signup

--- lib/pages/landing_page.dart
+++ lib/pages/landing_page.dart
@@ -23,91 +23,145 @@ class _LoginPageState extends State<LoginPage> {
   Widget build(BuildContext context) {
     return Scaffold(
         backgroundColor: Colors.white,
-        appBar: AppBar(
-          title: Text('Feel da beats'),
-        ),
-        body: SingleChildScrollView(
-          child: Padding(
-            padding: const EdgeInsets.all(16),
-            child:
-                Column(mainAxisAlignment: MainAxisAlignment.center, children: [
-              TextField(
-                keyboardType: TextInputType.emailAddress,
-                onChanged: (value) {
-                  email = value;
-                },
-                decoration: InputDecoration(
-                  hintText: "Enter your email",
-                  filled: true,
-                  fillColor: Colors.white,
-                  border: OutlineInputBorder(),
+        resizeToAvoidBottomInset: false,
+        // appBar: AppBar(
+        //   title: Text('Feel da beats'),
+        // ),
+        body: Container(
+          decoration: BoxDecoration(
+            image: DecorationImage(
+                image: AssetImage('assets/vector138.png'),
+                fit: BoxFit.fitWidth,
+                alignment: Alignment.topCenter
+                // Atur properti lain sesuai kebutuhan
                 ),
-              ),
-              SizedBox(height: 20),
-              TextField(
-                obscureText: true,
-                onChanged: (value) {
-                  password = value;
-                },
-                decoration: InputDecoration(
-                  hintText: "Enter your password",
-                  filled: true,
-                  fillColor: Colors.white,
-                  border: OutlineInputBorder(),
-                ),
-              ),
-              SizedBox(height: 20),
-              Text(_errorMessage, style: TextStyle(color: Colors.red)),
-              SizedBox(height: 10),
-              ElevatedButton(
-                onPressed: () async {
-                  setState(() {
-                    showSpinner = true;
-                  });
-                  try {
-                    await _auth
-                        .signInWithEmailAndPassword(
-                            email: email, password: password)
-                        .then((value) {
-                      setState(() {
-                        showSpinner = false;
-                      });
-                    });
-                    userManagement().navigateBasedOnRole(context);
-                  } catch (e) {
-                    print(e);
-                    if (e is FirebaseAuthException) {
-                      print("${e.code}");
-                      setState(() {
-                        _errorMessage =
-                            "Password or email incorrect, try again";
-                      });
-                    }
-                  }
-                },
-                child: Text('Login', style: TextStyle(color: Colors.white)),
-                style: ElevatedButton.styleFrom(
-                  backgroundColor: Colors.indigo[800],
-                  shape: RoundedRectangleBorder(
-                    borderRadius: BorderRadius.circular(0),
+          ),
+          child: Container(
+            decoration: BoxDecoration(
+                image: DecorationImage(
+                    image: AssetImage('assets/vector139.png'),
+                    fit: BoxFit.fitWidth,
+                    alignment: Alignment.bottomCenter
+                    // Atur properti lain sesuai kebutuhan
+                    )),
+            child: Column(
+              children: [
+                SizedBox(height: 75),
+                SizedBox(
+                  height: 200,
+                  width: double.infinity,
+                  child: Padding(
+                    padding: const EdgeInsets.all(16),
+                    child: Text('Login',
+                        textAlign: TextAlign.start,
+                        style: TextStyle(
+                          fontWeight: FontWeight.bold,
+                          fontSize: 30,
+                        )),
                   ),
-                  fixedSize: Size(200, 50),
                 ),
-              ),
-              SizedBox(height: 100),
-              Text("Don't have an account?"),
-              GestureDetector(
-                  onTap: () {
-                    Navigator.pushReplacement(context,
-                        MaterialPageRoute(builder: (context) => SignUpPage()));
-                    print('signup');
-                  },
-                  child: Text(
-                    'Sign Up',
-                    style: TextStyle(
-                        color: Colors.blue, fontWeight: FontWeight.bold),
-                  ))
-            ]),
+                Padding(
+                  padding: const EdgeInsets.all(30),
+                  child: Column(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: [
+                        TextField(
+                          keyboardType: TextInputType.emailAddress,
+                          onChanged: (value) {
+                            email = value;
+                          },
+                          decoration: InputDecoration(
+                            enabledBorder: OutlineInputBorder(
+                                borderSide: BorderSide(
+                                    width: 3,
+                                    color: Color.fromRGBO(103, 80, 163, 1))),
+                            hintText: "Enter your email",
+                            filled: true,
+                            fillColor: Colors.white,
+                            border: OutlineInputBorder(),
+                          ),
+                        ),
+                        SizedBox(height: 20),
+                        TextField(
+                          obscureText: true,
+                          onChanged: (value) {
+                            password = value;
+                          },
+                          decoration: InputDecoration(
+                            enabledBorder: OutlineInputBorder(
+                                borderSide: BorderSide(
+                                    width: 3,
+                                    color: Color.fromRGBO(103, 80, 163, 1))),
+                            hintText: "Enter your password",
+                            filled: true,
+                            fillColor: Colors.white,
+                            border: OutlineInputBorder(),
+                          ),
+                        ),
+                        SizedBox(height: 20),
+                        Text(_errorMessage,
+                            style: TextStyle(color: Colors.red)),
+                        SizedBox(height: 10),
+                        ElevatedButton(
+                          onPressed: () async {
+                            setState(() {
+                              showSpinner = true;
+                            });
+                            try {
+                              await _auth
+                                  .signInWithEmailAndPassword(
+                                      email: email, password: password)
+                                  .then((value) {
+                                setState(() {
+                                  showSpinner = false;
+                                });
+                              });
+                              userManagement().navigateBasedOnRole(context);
+                            } catch (e) {
+                              print(e);
+                              if (e is FirebaseAuthException) {
+                                print("${e.code}");
+                                setState(() {
+                                  _errorMessage =
+                                      "Password or email incorrect, try again";
+                                });
+                              }
+                            }
+                          },
+                          child: Text('Login',
+                              style: TextStyle(color: Colors.white)),
+                          style: ElevatedButton.styleFrom(
+                              backgroundColor: Color.fromRGBO(66, 87, 154, 1),
+                              shape: RoundedRectangleBorder(
+                                borderRadius: BorderRadius.circular(0),
+                              ),
+                              fixedSize: Size(200, 50),
+                              side: BorderSide(
+                                  width: 3,
+                                  color: Color.fromRGBO(103, 80, 163, 1))),
+                        ),
+                        SizedBox(height: 180),
+                        Text("Don't have an account?",
+                            style: TextStyle(
+                                fontSize: 14, fontWeight: FontWeight.bold)),
+                        GestureDetector(
+                            onTap: () {
+                              Navigator.pushReplacement(
+                                  context,
+                                  MaterialPageRoute(
+                                      builder: (context) => SignUpPage()));
+                              print('signup');
+                            },
+                            child: Text(
+                              'Sign Up',
+                              style: TextStyle(
+                                  color: Color.fromRGBO(66, 87, 154, 1),
+                                  fontWeight: FontWeight.bold),
+                            ))
+                      ]),
+                ),
+              ],
+            ),
           ),
         ));
   }
--- lib/pages/register.dart
+++ lib/pages/register.dart
@@ -22,112 +22,179 @@ class _SignUpPageState extends State<SignUpPage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      resizeToAvoidBottomInset: true,
+      resizeToAvoidBottomInset: false,
       backgroundColor: Colors.white,
-      appBar: AppBar(
-        title: Text('Feel da beats'),
-      ),
-      body: SingleChildScrollView(
-        child: Padding(
-          padding: const EdgeInsets.all(16),
-          child: Column(mainAxisAlignment: MainAxisAlignment.center, children: [
-            TextField(
-              keyboardType: TextInputType.emailAddress,
-              onChanged: (value) {
-                username = value;
-              },
-              decoration: InputDecoration(
-                hintText: "Enter your username",
-                filled: true,
-                fillColor: Colors.white,
-                border: OutlineInputBorder(),
+      // appBar: AppBar(
+      //   title: Text('Feel da beats'),
+      // ),
+      body: Container(
+        decoration: BoxDecoration(
+          image: DecorationImage(
+              image: AssetImage('assets/vector138.png'),
+              fit: BoxFit.fitWidth,
+              alignment: Alignment.topCenter
+              // Atur properti lain sesuai kebutuhan
               ),
-            ),
-            SizedBox(height: 20),
-            TextField(
-              keyboardType: TextInputType.emailAddress,
-              onChanged: (value) {
-                email = value;
-              },
-              decoration: InputDecoration(
-                hintText: "Enter your email",
-                filled: true,
-                fillColor: Colors.white,
-                border: OutlineInputBorder(),
-              ),
-            ),
-            SizedBox(height: 20),
-            TextField(
-              obscureText: true,
-              onChanged: (value) {
-                password = value;
-              },
-              decoration: InputDecoration(
-                hintText: "Enter your password",
-                filled: true,
-                fillColor: Colors.white,
-                border: OutlineInputBorder(),
-              ),
-            ),
-            SizedBox(height: 20),
-            Text(_errorMessage, style: TextStyle(color: Colors.red)),
-            SizedBox(height: 10),
-            ElevatedButton(
-              onPressed: () async {
-                setState(() {
-                  showSpinner = true;
-                });
-                if (username.isEmpty || email.isEmpty || password.isEmpty) {
-                  setState(() {
-                    _errorMessage = "Field can't empty, fill the field!";
-                  });
-                } else {
-                  try {
-                    await _auth
-                        .createUserWithEmailAndPassword(
-                            email: email, password: password)
-                        .then((value) {
-                      setState(() {
-                        showSpinner = false;
-                      });
-                    });
-                    Navigator.pushReplacement(context,
-                        MaterialPageRoute(builder: (context) => MyHomePage()));
-                  } catch (e) {
-                    if (e is FirebaseAuthException) {
-                      setState(() {
-                        _errorMessage = e.message!;
-                      });
-                    }
-                  }
-                  print(FirebaseAuth.instance.currentUser?.uid);
-                  userManagement().saveUserDataToFirestore(
-                      FirebaseAuth.instance.currentUser!.uid, email, username);
-                }
-              },
-              child: Text('Sign Up', style: TextStyle(color: Colors.white)),
-              style: ElevatedButton.styleFrom(
-                backgroundColor: Colors.indigo[800],
-                shape: RoundedRectangleBorder(
-                  borderRadius: BorderRadius.circular(0),
+        ),
+        child: Container(
+          decoration: BoxDecoration(
+            image: DecorationImage(
+                image: AssetImage('assets/vector139.png'),
+                fit: BoxFit.fitWidth,
+                alignment: Alignment.bottomCenter
+                // Atur properti lain sesuai kebutuhan
                 ),
-                fixedSize: Size(200, 50),
+          ),
+          child: Column(
+            children: [
+              SizedBox(height: 75),
+              Padding(
+                padding: EdgeInsets.all(16),
+                child: SizedBox(
+                  height: 130,
+                  width: double.infinity,
+                  child: Text(
+                    'Create Account',
+                    style: TextStyle(
+                        fontFamily: 'Poppins',
+                        fontWeight: FontWeight.bold,
+                        fontSize: 30),
+                    textAlign: TextAlign.start,
+                  ),
+                ),
               ),
-            ),
-            SizedBox(height: 100),
-            Text("Already have an account?"),
-            GestureDetector(
-                onTap: () {
-                  Navigator.pushReplacement(context,
-                      MaterialPageRoute(builder: (context) => LoginPage()));
-                  print('Login');
-                },
-                child: Text(
-                  'Login',
-                  style: TextStyle(
-                      color: Colors.blue, fontWeight: FontWeight.bold),
-                ))
-          ]),
+              Padding(
+                padding: const EdgeInsets.all(30),
+                child: Column(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      TextField(
+                        keyboardType: TextInputType.emailAddress,
+                        onChanged: (value) {
+                          username = value;
+                        },
+                        decoration: InputDecoration(
+                          enabledBorder: OutlineInputBorder(
+                              borderSide: BorderSide(
+                                  width: 3,
+                                  color: Color.fromRGBO(103, 80, 163, 1))),
+                          hintText: "Enter your username",
+                          filled: true,
+                          fillColor: Colors.white,
+                          border: OutlineInputBorder(),
+                        ),
+                      ),
+                      SizedBox(height: 20),
+                      TextField(
+                        keyboardType: TextInputType.emailAddress,
+                        onChanged: (value) {
+                          email = value;
+                        },
+                        decoration: InputDecoration(
+                          enabledBorder: OutlineInputBorder(
+                              borderSide: BorderSide(
+                                  width: 3,
+                                  color: Color.fromRGBO(103, 80, 163, 1))),
+                          hintText: "Enter your email",
+                          filled: true,
+                          fillColor: Colors.white,
+                          border: OutlineInputBorder(),
+                        ),
+                      ),
+                      SizedBox(height: 20),
+                      TextField(
+                        obscureText: true,
+                        onChanged: (value) {
+                          password = value;
+                        },
+                        decoration: InputDecoration(
+                          enabledBorder: OutlineInputBorder(
+                              borderSide: BorderSide(
+                                  width: 3,
+                                  color: Color.fromRGBO(103, 80, 163, 1))),
+                          hintText: "Enter your password",
+                          filled: true,
+                          fillColor: Colors.white,
+                          border: OutlineInputBorder(),
+                        ),
+                      ),
+                      SizedBox(height: 20),
+                      Text(_errorMessage, style: TextStyle(color: Colors.red)),
+                      SizedBox(height: 10),
+                      ElevatedButton(
+                        onPressed: () async {
+                          setState(() {
+                            showSpinner = true;
+                          });
+                          if (username.isEmpty ||
+                              email.isEmpty ||
+                              password.isEmpty) {
+                            setState(() {
+                              _errorMessage =
+                                  "Field can't empty, fill the field!";
+                            });
+                          } else {
+                            try {
+                              await _auth
+                                  .createUserWithEmailAndPassword(
+                                      email: email, password: password)
+                                  .then((value) {
+                                setState(() {
+                                  showSpinner = false;
+                                });
+                              });
+                              Navigator.pushReplacement(
+                                  context,
+                                  MaterialPageRoute(
+                                      builder: (context) => MyHomePage()));
+                            } catch (e) {
+                              if (e is FirebaseAuthException) {
+                                setState(() {
+                                  _errorMessage = e.message!;
+                                });
+                              }
+                            }
+                            print(FirebaseAuth.instance.currentUser?.uid);
+                            userManagement().saveUserDataToFirestore(
+                                FirebaseAuth.instance.currentUser!.uid,
+                                email,
+                                username);
+                          }
+                        },
+                        child: Text('Sign Up',
+                            style: TextStyle(color: Colors.white)),
+                        style: ElevatedButton.styleFrom(
+                            backgroundColor: Color.fromRGBO(66, 87, 154, 1),
+                            shape: RoundedRectangleBorder(
+                              borderRadius: BorderRadius.circular(0),
+                            ),
+                            fixedSize: Size(200, 50),
+                            side: BorderSide(
+                                width: 3,
+                                color: Color.fromRGBO(103, 80, 163, 1))),
+                      ),
+                      SizedBox(height: 120),
+                      Text("Already have an account?",
+                          style: TextStyle(
+                              fontSize: 14, fontWeight: FontWeight.bold)),
+                      GestureDetector(
+                          onTap: () {
+                            Navigator.pushReplacement(
+                                context,
+                                MaterialPageRoute(
+                                    builder: (context) => LoginPage()));
+                            print('Login');
+                          },
+                          child: Text(
+                            'Login',
+                            style: TextStyle(
+                                color: Color.fromRGBO(66, 87, 154, 1),
+                                fontWeight: FontWeight.bold),
+                          ))
+                    ]),
+              ),
+            ],
+          ),
         ),
       ),
     );
